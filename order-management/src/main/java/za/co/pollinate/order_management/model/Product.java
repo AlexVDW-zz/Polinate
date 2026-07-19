0ad7d1dabb16129d90b0460b36084a5f0ddd6ad6
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-19 15:51:44 +0200

    Added Product Entity and Repository

---
+++ order-management/src/main/java/za/co/pollinate/order_management/model/Product.java
@@ -0,0 +1,29 @@
+package za.co.pollinate.order_management.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "product")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+}
