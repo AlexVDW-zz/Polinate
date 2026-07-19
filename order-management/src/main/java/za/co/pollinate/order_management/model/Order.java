7f086bce5bf8b6f8f20ac2d9453228ef86ccf02c
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-19 15:47:13 +0200

    Added Order Entity and Repository

---
+++ order-management/src/main/java/za/co/pollinate/order_management/model/Order.java
@@ -0,0 +1,41 @@
+package za.co.pollinate.order_management.model;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private List<OrderItem> orderItems;
+}
