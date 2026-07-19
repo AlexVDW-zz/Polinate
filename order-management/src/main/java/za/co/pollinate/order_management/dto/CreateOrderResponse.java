e8c47389a7ea7298aa6034361220e5005d413ce5
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-19 16:12:19 +0200

    Added Response object for Creating Product

---
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderResponse.java
@@ -0,0 +1,15 @@
+package za.co.pollinate.order_management.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateOrderResponse implements Serializable {
+    private Long orderId;
+    private String message;    
+}
---
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateProductResponse.java
@@ -0,0 +1,14 @@
+package za.co.pollinate.order_management.dto;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateProductResponse implements Serializable {
+    private Long productId;
+    private String message;
+}
