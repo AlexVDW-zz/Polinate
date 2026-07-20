a849c0f11775e585608ebae7df0e16c38e24d6e3
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-20 10:23:23 +0200

    Refactor - fixed typo

--- order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderResponse.java
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderResponse.java
@@ -1,15 +1,14 @@
 package za.co.pollinate.order_management.dto;
 
-import java.io.Serializable;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateOrderResponse implements Serializable {
     private Long orderId;
-    private String message;    
+    private String message;
 }
--- order-management/src/main/java/za/co/pollinate/order_management/dto/CreateProductResponse.java
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateProductResponse.java
@@ -1,6 +1,7 @@
 package za.co.pollinate.order_management.dto;
 
 import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProductResponse implements Serializable {
-    private Long productId;
-    private String message;
+    private Long orderId;
+    private String message; 
 }
