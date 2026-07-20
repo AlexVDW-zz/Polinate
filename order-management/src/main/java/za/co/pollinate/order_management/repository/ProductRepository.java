e54e7b50a4dba7e5a1c4b2f88e1574bad800137c
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-20 08:29:09 +0200

    Fixed file type on Repository classes

--- order-management/src/main/java/za/co/pollinate/order_management/repository/ProductRepository.java
+++ order-management/src/main/java/za/co/pollinate/order_management/repository/ProductRepository.java
@@ -2,7 +2,8 @@ package za.co.pollinate.order_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import za.co.pollinate.order_management.model.Order;
+
+import za.co.pollinate.order_management.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
