61153bd8db6d6b841dd3dbc6db31953b79d263ba
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-20 15:18:10 +0200

    Removed incorrect service annotation after refactor

--- order-management/src/main/java/za/co/pollinate/order_management/service/ProductService.java
+++ order-management/src/main/java/za/co/pollinate/order_management/service/ProductService.java
@@ -1,11 +1,9 @@
 package za.co.pollinate.order_management.service;
 
 import za.co.pollinate.order_management.dto.ProductDTO;
-import org.springframework.stereotype.Service;
 import java.util.List;
 import java.math.BigDecimal;
 
-@Service
 public interface ProductService {
     Long createProduct(String name, BigDecimal price);
     ProductDTO getProductById(Long id);
