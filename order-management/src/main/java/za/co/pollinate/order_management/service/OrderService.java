3478c22cbea7282c1b8bfd813bc4723c5da5f7e2
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-20 11:34:04 +0200

    removed unused imports

--- order-management/src/main/java/za/co/pollinate/order_management/service/OrderService.java
+++ order-management/src/main/java/za/co/pollinate/order_management/service/OrderService.java
@@ -1,22 +1,8 @@
 package za.co.pollinate.order_management.service;
 
-import org.springframework.stereotype.Service;
-import za.co.pollinate.order_management.model.OrderItem;
-import za.co.pollinate.order_management.model.Order;
-import za.co.pollinate.order_management.model.Product;
 import za.co.pollinate.order_management.dto.OrderDTO;
-import za.co.pollinate.order_management.dto.OrderItemDTO;
-import za.co.pollinate.order_management.dto.ProductDTO;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.math.BigDecimal;
-
-import za.co.pollinate.order_management.repository.OrderRepository;
-import za.co.pollinate.order_management.repository.ProductRepository;
-
-import org.springframework.transaction.annotation.Transactional;
-
 import za.co.pollinate.order_management.dto.CreateOrderRequest;
 
 public interface OrderService {
