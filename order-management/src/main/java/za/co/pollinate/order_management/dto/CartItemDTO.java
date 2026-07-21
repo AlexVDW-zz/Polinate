e629136575f298ac676bfcec37dddcbe4f9b5b91
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-21 08:25:38 +0200

    Fixed Cart Items Validation

--- order-management/src/main/java/za/co/pollinate/order_management/dto/CartItemDTO.java
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CartItemDTO.java
@@ -3,6 +3,7 @@ package za.co.pollinate.order_management.dto;
 import java.io.Serializable;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartItemDTO implements Serializable {
+    @NotNull(message="Product Id is required")
     @Min(value=1, message="Product Id needs to be a positive integer value")
     private Long productId;
 
+    @NotNull(message="Quantity is required")
     @Min(value=1, message="Quantity needs to be a positive integer value")
     private Integer quantity;
 }
--- order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderRequest.java
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderRequest.java
@@ -7,11 +7,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.Valid;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateOrderRequest implements Serializable {
     @NotEmpty(message="Order cannot have no cart items linked")
+    @Valid
     private List<CartItemDTO> cartItems;
 }
