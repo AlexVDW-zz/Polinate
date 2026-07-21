a3c1d1dc369aef0c18dac961bb5ae05d94e99c9f
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-21 08:08:34 +0200

    Added pagination to all orders lookup

--- order-management/src/main/java/za/co/pollinate/order_management/controller/OrderController.java
+++ order-management/src/main/java/za/co/pollinate/order_management/controller/OrderController.java
@@ -2,12 +2,16 @@ package za.co.pollinate.order_management.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -19,6 +23,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import za.co.pollinate.order_management.dto.OrderDTO;
+import za.co.pollinate.order_management.model.Product;
 import za.co.pollinate.order_management.dto.CreateOrderResponse;
 import za.co.pollinate.order_management.dto.CreateOrderRequest;
 import za.co.pollinate.order_management.service.OrderService;
@@ -97,9 +102,12 @@ public class OrderController {
     })
     @GetMapping("/list-orders")
     @Cacheable(value = "allOrders", key = "'all-orders'")
-    public ResponseEntity<BaseResponse<List<OrderDTO>>> getAllOrders() {
+    public ResponseEntity<BaseResponse<List<OrderDTO>>> getAllOrders(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
         log.info("Received request to list all orders");
-        List<OrderDTO> orders = orderService.getAllOrders();
+        Pageable pageable = PageRequest.of(page, size); 
+
+        List<OrderDTO> orders = orderService.getAllOrders(pageable);
         BaseResponse<List<OrderDTO>> baseResponse = new BaseResponse<>(null, orders);
         log.info("Orders listed successfully");
         return ResponseEntity.ok(baseResponse);
