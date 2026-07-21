3b086886aee5e1e99e1bec03bcabf1f504ed6a49
AlexanderVDW <alexander.1.vdwesthuizen@gmail.com>
2026-07-21 07:35:22 +0200

    Added Order validation
    Order needs to have cart items

--- order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderRequest.java
+++ order-management/src/main/java/za/co/pollinate/order_management/dto/CreateOrderRequest.java
@@ -6,10 +6,12 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotEmpty;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateOrderRequest implements Serializable {
+    @NotEmpty(message="Order cannot have no cart items linked")
     private List<CartItemDTO> cartItems;
 }
